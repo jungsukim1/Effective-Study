2aa3866e40b00fbc20ecdeb9dc4a0552a48e699e
jungsukim1 <jungsukim2609@gmail.com>
2022-01-18 00:13:41 -0700

    Starting

---
+++ index.js
@@ -0,0 +1,12 @@
+import React, { Compnent } from 'react';
+// var myName = window.prompt("Do you want to play the game?");
+
+// console.log(myName);
+
+document.getElementById("myButton").onclick = function(){
+
+    var myName = document.getElementById("myText").value;
+
+    console.log("hello",myName);
+
+}
